0cd48e9f0e13a6dbee57577729b85d54c0db6a77
Alexandra <alexandrazapalii@yahoo.com>
2023-10-15 20:21:37 +0300

    am modificat ceva

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 function addToDo() {
-    var taskInput = document.getElementById('taskInput');
-    var taskText = taskInput.value;
+    const taskInput = document.getElementById('taskInput');
+    const taskText = taskInput.value;
 
     if (taskText.trim() === '') {
         alert('Please enter a task.');
         return;
     }
 
-    var taskList = document.getElementById('taskList');
-    var taskItem = document.createElement('li');
+    const taskList = document.getElementById('taskList');
+    const taskItem = document.createElement('li');
     taskItem.appendChild(document.createTextNode(taskText));
     taskList.appendChild(taskItem);
 
